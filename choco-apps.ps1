d3f608bbadd70f1dddc3d120267cbc731ad38c03
Daniel F Persson <daniel.f.persson@atea.se>
2019-06-12 14:26:23 +0200

    Hej!

---
+++ choco-apps.ps1
@@ -0,0 +1,22 @@
+choco install `
+powershell-core `
+notepadplusplus `
+googlechrome `
+7zip `
+adobereader `
+flashplayerplugin `
+jre8 `
+flashplayeractivex `
+firefox `
+opera `
+powershell `
+vscode `
+putty.install `
+dotnet4.7.1 `
+totalcommander `
+keepass.install `
+royalts `
+windows-admin-center `
+teamviewer.host `
+angryip `
+-dyv
