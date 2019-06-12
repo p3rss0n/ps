d4d8edf79722606254bbc9715eb4682c02e1796c
Daniel F Persson <daniel.f.persson@atea.se>
2019-06-12 14:29:57 +0200

    2019-06-12

---
+++ modules.ps1
@@ -0,0 +1,6 @@
+Install-PackageProvider -Name "Nuget" -Verbose -Force
+Install-Module PScribo -Verbose -Force
+Install-Module VMware.PowerCLI -Verbose -Force
+Install-Module AsBuiltReport -Verbose -Force
+install-module AzureAD -Verbose -Force
+install-module MSOnline -Verbose -Force
--- post-install.ps1
+++ post-install.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/p3rss0n/ps/blob/master/choco-apps.ps1'))
