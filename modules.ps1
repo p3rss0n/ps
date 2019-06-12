9904d036d79bc4994751aebb0ef7bfe572039c5f
Daniel F Persson <daniel.f.persson@atea.se>
2019-06-12 14:42:14 +0200

    test123

--- modules.ps1
+++ modules.ps1
@@ -1,6 +1,6 @@
-Install-PackageProvider -Name "Nuget" -Verbose -Force
-Install-Module PScribo -Verbose -Force
-Install-Module VMware.PowerCLI -Verbose -Force
-Install-Module AsBuiltReport -Verbose -Force
-install-module AzureAD -Verbose -Force
-install-module MSOnline -Verbose -Force
+Install-PackageProvider -Name "Nuget" -Verbose
+Install-Module PScribo -Verbose
+Install-Module VMware.PowerCLI -Verbose
+Install-Module AsBuiltReport -Verbose
+install-module AzureAD -Verbose
+install-module MSOnline -Verbose
